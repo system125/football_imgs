f30a90a5e6b92d195fc1e4487f9d9a5d0372fe41
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2025-07-16 16:02:04 +0200

    implemented top headers

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 
+from dataclasses import dataclass
 from dotenv import load_dotenv
 import os 
+import requests
 
 load_dotenv()
 
@@ -14,4 +16,46 @@ Auth_headers = {
 }
 
 
-print(f"{API_KEY}")
+
+@dataclass
+class LeagueInfo:
+    name:str
+    country:str 
+    logo_url:str
+
+def get_league_info(id:int):
+    """
+    Loads league roster from api 
+    and returns the roster
+    """
+
+    querystring={"leagueid":f"{id}"}
+    if API_URL:
+        response = requests.get(
+                        f"{API_URL}/football-get-league-detail",
+                        headers=Auth_headers,
+                        params=querystring
+                        )
+        logo_response = requests.get(
+            f"{API_URL}/football-get-league-logo",
+            headers=Auth_headers,
+            params=querystring
+        )
+        if response.status_code != 200 or logo_response.status_code != 200:
+            raise Exception("Error!! Loading league data!")
+        league_data = response.json()["response"]["leagues"]
+
+        name = league_data["name"]
+        country = league_data["country"]
+        logo = logo_response.json()['response']['url']
+
+        return LeagueInfo(
+            name=name,
+            country=country,
+            logo_url=logo
+        )
+        
+        
+        
+
+        
--- main.py
+++ main.py
@@ -3,18 +3,28 @@ import api
 import webview
 from jinja2 import Environment, FileSystemLoader
 import os 
+import api 
 
 env = Environment(loader=FileSystemLoader('templates'))
 
-id = 47
 
 with open("styles/styles.css") as f:
     css = f.read()
     f.close()
 
 print(f"{css}")
+
+id = 47
+league_data = api.get_league_info(id = id )
+
 template = env.get_template("index.jinja2")
-html = template.render(title="MyApp",header="Welcome to A4 Page",interactive=True,custom_css=css)
+html = template.render(
+    title=league_data.name, #type: ignore
+    header="Welcome to A4 Page",
+    custom_css=css,
+    league_img=league_data.logo_url #type: ignore
+)
+
 print(html)
 # with open("test.html",'w') as f:
 #     f.writelines(html)
