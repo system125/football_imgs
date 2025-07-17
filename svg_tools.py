fc66079775ec71ee98b852aad3f9ddc076462fd1
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2025-07-17 11:51:06 +0200

    converted svg to outlines

--- svg_tools.py
+++ svg_tools.py
@@ -5,14 +5,19 @@ from xml.dom import minidom
 SVG_NS = "http://www.w3.org/2000/svg"
 ET.register_namespace("", SVG_NS)
 
+from xml.etree import ElementTree as ET
+from xml.dom import minidom
+
+SVG_NS = "http://www.w3.org/2000/svg"
+ET.register_namespace("", SVG_NS)
+
 def shape_to_path(element):
-    """Convert basic SVG shapes to <path d="..."> elements."""
     tag = element.tag.split("}")[-1]
     
     if tag == 'path':
-        return element  # already a path
-
-    if tag == 'rect':
+        d = element.get('d')
+    
+    elif tag == 'rect':
         x = float(element.get('x', 0))
         y = float(element.get('y', 0))
         w = float(element.get('width', 0))
@@ -23,18 +28,14 @@ def shape_to_path(element):
         cx = float(element.get('cx', 0))
         cy = float(element.get('cy', 0))
         r = float(element.get('r', 0))
-        d = f"M{cx - r},{cy} " \
-            f"a{r},{r} 0 1,0 {2*r},0 " \
-            f"a{r},{r} 0 1,0 {-2*r},0"
+        d = f"M{cx - r},{cy} a{r},{r} 0 1,0 {2*r},0 a{r},{r} 0 1,0 {-2*r},0"
     
     elif tag == 'ellipse':
         cx = float(element.get('cx', 0))
         cy = float(element.get('cy', 0))
         rx = float(element.get('rx', 0))
         ry = float(element.get('ry', 0))
-        d = f"M{cx - rx},{cy} " \
-            f"a{rx},{ry} 0 1,0 {2*rx},0 " \
-            f"a{rx},{ry} 0 1,0 {-2*rx},0"
+        d = f"M{cx - rx},{cy} a{rx},{ry} 0 1,0 {2*rx},0 a{rx},{ry} 0 1,0 {-2*rx},0"
     
     elif tag == 'line':
         x1 = float(element.get('x1', 0))
@@ -60,26 +61,26 @@ def shape_to_path(element):
     else:
         return None
 
-    # Create new <path> element with same style attributes
+    # Create new <path> element with outline style
     path_el = ET.Element(f"{{{SVG_NS}}}path")
     path_el.set("d", d)
-    for attr in element.attrib:
-        if attr not in ['x', 'y', 'cx', 'cy', 'r', 'rx', 'ry', 'width', 'height', 'x1', 'y1', 'x2', 'y2', 'points']:
-            path_el.set(attr, element.attrib[attr])
+    path_el.set("stroke", "black")
+    path_el.set("stroke-width", "1")
+    path_el.set("fill", "none")
     return path_el
 
 def extract_outline_svg(svg_string):
     root = ET.fromstring(svg_string)
-    
-    # Create new SVG root
     new_svg = ET.Element(f"{{{SVG_NS}}}svg", root.attrib)
 
-    # Copy viewBox, width, height
+    # Copy dimensions
     for attr in ['viewBox', 'width', 'height']:
         if attr in root.attrib:
             new_svg.set(attr, root.attrib[attr])
+    
+    new_svg.set('width','210')
+    new_svg.set('height','210')
 
-    # Outlineable elements
     tag_names = ['path', 'rect', 'circle', 'ellipse', 'line', 'polyline', 'polygon']
 
     for elem in root.iter():
@@ -89,8 +90,16 @@ def extract_outline_svg(svg_string):
             if path_elem is not None:
                 new_svg.append(path_elem)
 
-    # Beautify
     rough_string = ET.tostring(new_svg, 'utf-8')
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
+
+if __name__=="__main__":
+    with open("assets/original.svg",'r') as f:
+        svg = f.read()
+
+    outline = extract_outline_svg(svg)
+
+    with open("assets/outlines.svg",'w') as f:
+        f.write(outline)
